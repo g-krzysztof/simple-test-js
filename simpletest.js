c8c8125bf2c38b2c5bb3a642561851bd7a5786eb
Krzysztof Gryc <hello@krzysztofg.pl>
2019-07-12 15:39:16 +0200

    add collapse error feature

--- simpletest.js
+++ simpletest.js
@@ -46,8 +46,9 @@ var TinyTest = {
                 console.log("%c" + testName, "color: green;");
             } catch (e) {
                 failures++;
-                console.log('%c' + testName + e, 'color: red;');
+                console.groupCollapsed('%c' + testName + e, 'color: red;');
                 console.error(e.stack);
+                console.groupEnd();
             }
         }
         setTimeout(function() { // Give document a chance to complete
